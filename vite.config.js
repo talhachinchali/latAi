ceb5d4f42e244576597eb780a1e0baf402c99578
Talha M Chinchali <113890152+talhachinchali@users.noreply.github.com>
2025-04-09 10:22:29 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
+     allowedHosts: ['latai.onrender.com'],
     host: true,
     strictPort: true,
     headers: {
